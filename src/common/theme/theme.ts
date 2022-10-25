18b3d411fee39037c5c2303efb6385824a349ce9
Navar Kartalian <alpha.hunter88@gmail.com>
2022-10-25 17:47:31 -0300

    ✨ Add codegen and theme

---
+++ src/common/theme/theme.ts
@@ -0,0 +1,15 @@
+import { extendTheme } from "@chakra-ui/react"
+
+export const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: 'black',
+        color: 'white'
+      }
+    }
+  },
+  fonts: {
+    body: `'Poppins', sans-serif`
+  }
+});
